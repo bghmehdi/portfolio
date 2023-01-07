f83999b1cd1e97aa7b33f4d90eba41d6111fae09
Mehdi Boughaleb <bgh84000@gmail.com>
2023-01-07 17:56:32 +0100

    polish

--- script.js
+++ script.js
@@ -20,13 +20,14 @@ const root = document.querySelector(':root');
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
-        root.style.setProperty('--primary-color', '#f7f6f2');
-        root.style.setProperty('--secondary-color', '#1b2430');
+        root.style.setProperty('--primary-color', '#1b2430');
+        root.style.setProperty('--secondary-color', '#f7f6f2');
+        
        /*  body.style.transition = '2s';
         navbar.style.transition = '2s'; */
     }else{
-        root.style.setProperty('--primary-color', '#1b2430');
-        root.style.setProperty('--secondary-color', '#f7f6f2');
+        root.style.setProperty('--primary-color', '#f7f6f2');
+        root.style.setProperty('--secondary-color', '#1b2430');
         /* body.style.transition = '2s'; 
          navbar.style.transition = '2s'; */
     }
