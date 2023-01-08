1a35ebb6bcd1fe0c1773995bfb28a5c2e37ca68e
Mehdi Boughaleb <bgh84000@gmail.com>
2023-01-08 21:30:00 +0100

    more polish

--- script.js
+++ script.js
@@ -13,23 +13,44 @@ function myFunction() {
     }
 }
 
-const toggle = document.getElementById('toggleDark');
-const body = document.querySelector('body');
-const root = document.querySelector(':root');
+const toggle = document.getElementById("toggleDark");
+const body = document.querySelector("body");
+const root = document.querySelector(":root");
 
-toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill')){
-        root.style.setProperty('--primary-color', '#1b2430');
-        root.style.setProperty('--secondary-color', '#f7f6f2');
-        
-       /*  body.style.transition = '2s';
+toggle.addEventListener("click", function () {
+    this.classList.toggle("bi-moon");
+    if (this.classList.toggle("bi-brightness-high-fill")) {
+        root.style.setProperty("--primary-color", "#1b2430");
+        root.style.setProperty("--secondary-color", "#f7f6f2");
+
+        /*  body.style.transition = '2s';
         navbar.style.transition = '2s'; */
-    }else{
-        root.style.setProperty('--primary-color', '#f7f6f2');
-        root.style.setProperty('--secondary-color', '#1b2430');
+    } else {
+        root.style.setProperty("--primary-color", "#f7f6f2");
+        root.style.setProperty("--secondary-color", "#1b2430");
         /* body.style.transition = '2s'; 
          navbar.style.transition = '2s'; */
     }
 });
 
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", activeMenu);
+function activeMenu() {
+    let scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 200;
+        let sectionId = current.getAttribute("id");
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector(".navbar a[href*=" + sectionId + "]")
+                .classList.add("active");
+        } else {
+            document
+                .querySelector(".navbar a[href*=" + sectionId + "]")
+                .classList.remove("active");
+        }
+    });
+}
