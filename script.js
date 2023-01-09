5660aac799f62a1b59a71e621e13a309c7e5f398
Mehdi Boughaleb <bgh84000@gmail.com>
2023-01-09 17:27:01 +0100

    extra polish

--- script.js
+++ script.js
@@ -22,12 +22,13 @@ toggle.addEventListener("click", function () {
     if (this.classList.toggle("bi-brightness-high-fill")) {
         root.style.setProperty("--primary-color", "#1b2430");
         root.style.setProperty("--secondary-color", "#f7f6f2");
-
+        root.style.setProperty("--accent-color", "#8ce7f4");
         /*  body.style.transition = '2s';
         navbar.style.transition = '2s'; */
     } else {
         root.style.setProperty("--primary-color", "#f7f6f2");
         root.style.setProperty("--secondary-color", "#1b2430");
+        root.style.setProperty("--accent-color", "#a10035");
         /* body.style.transition = '2s'; 
          navbar.style.transition = '2s'; */
     }
